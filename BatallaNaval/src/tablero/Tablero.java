797cc6cb0f90bc4d01dd298e3eadb9b3adca180b
Emiliano Germani <emi_germani@hotmail.com>
2024-09-03 19:45:06 -0300

    Metodos de Tablero

--- BatallaNaval/src/tablero/Tablero.java
+++ BatallaNaval/src/tablero/Tablero.java
@@ -12,14 +12,14 @@ import piezas.*;
  */
 public class Tablero {
 
-    private Casilla[][] mapa;//por el momento lo dejamos como un int pero deberia de ser algo relacionados con las piezas por polimorfismo
+    private Casilla[][] mapa;
     private int cantidadNave;
     private Portaaviones[] portaviones;
     private Buque[] buques;
     private Submarino[] submarinos;
     private Crucero[] cruceros;
     private Lancha[] lanchas;
-    private int barcosHundidos; // es un contador
+    private int barcosHundidos; 
 
     public Tablero() {
     }
@@ -70,17 +70,65 @@ public class Tablero {
    //     this.barcosHundidos = barcosHundidos;
     }
 
-    
-
-    public void colocarNave() {
-
+    public Casilla[][] getMapa() {
+        return mapa;
     }
 
-    public void tiroEnemigo() {
-
+    public void verTableroDePiezas(int tamanio) { 
+        for (int i = 0; i < tamanio; i++) {
+            for (int j = 0; j < tamanio; j++) { //Recorro mi tablero para que muestre las posiciones de los barcos e islas
+                if (this.mapa[i][j].isAgua()){ //Si agua es True
+                    System.out.print("[ ]");
+                }else if(this.mapa[i][j].isIsla()){ //Si isla es True
+                    System.out.print("[I]");
+                }else{ //Si hay una nave
+                    System.out.print("[N]");
+                }
+            }
+            System.out.println("");
+        }
     }
 
-    public void marcarAtaque() {
-
+    public void verTableroDeTiros(int tamanio, Tablero tableroEnemigo) {
+        for (int i = 0; i < tamanio; i++) {
+            for (int j = 0; j < tamanio; j++) { //Recorro el tablero del enemigo para ver donde he dado tiros
+                if (tableroEnemigo.getMapa()[i][j].isHit()){ //Si hit es True
+                    if (tableroEnemigo.getMapa()[i][j].isAgua()){ //Si agua es True
+                    System.out.print("[O]");
+                    }else if(tableroEnemigo.getMapa()[i][j].isIsla()){ //Si isla es True
+                        System.out.print("[O]");
+                    }else{ //Si hay una nave
+                        System.out.print("[X]");
+                    }
+                }else{ //Si no se disparo en esa casilla
+                    System.out.print("[ ]");
+                }
+            }
+            System.out.println("");
+        }
     }
+
+    public void marcarAtaque(int i, int j, Tablero tableroEnemigo) {
+        tableroEnemigo.getMapa()[i][j].setHit(true); //Ingreso a la casilla elegida en el tablero enemigo y marco hit = true
+        tableroEnemigo.getMapa()[i][j].getNave().RecibirDaño(); //Ingreso a la referencia de la nave de esa casilla y utilizo RecibirDaño()
+    }
+
+    public void barcosASalvo(){ //Muestra en pantalla la cantidad de barcos de cada tipo aun a salvo
+        if (this.portaviones.length>0){
+            System.out.println("Portaviones: "+ this.portaviones.length);
+        }
+        if (this.buques.length>0){
+            System.out.println("Buques: "+ this.buques.length);
+        }
+        if (this.submarinos.length>0){
+            System.out.println("Submarinos: "+ this.submarinos.length);
+        }
+        if (this.cruceros.length>0){
+            System.out.println("Cruceros: "+ this.cruceros.length);
+        }
+        if (this.lanchas.length>0){
+            System.out.println("Lanchas: "+ this.lanchas.length);
+        }  
+    }
+
 }
